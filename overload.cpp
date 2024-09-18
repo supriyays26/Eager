948dee4a6236ee0678fd5bb60ff3b5c76f898e6e
supriya <R20200562.SUPRIYAYS@csereva.edu.in>
2024-09-18 17:35:00 +0530

    added file

---
+++ overload.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void add1(double x,double y){
+	cout<<"first"<<endl;
+}
+
+void add2(int x,int y){
+	cout<<"second"<<endl;
+}
+
+
+int main(){
+		add1(1.3,3.4);
+		add2(2,3);
+return 0;}
